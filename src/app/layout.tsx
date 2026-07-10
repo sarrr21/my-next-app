2777758602df278e906c9bc0989af7cec8e7931f
Sara Getnet <gs9425188@gmail.com>
2026-07-10 17:51:09 +0300

    Refactor font declarations in layout and update font variables in globals.css for improved consistency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,6 @@
 import type { Metadata } from "next";
-import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
-
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-  display: "swap",
-});
-
-const playfairDisplay = Playfair_Display({
-  variable: "--font-playfair",
-  subsets: ["latin"],
-  display: "swap",
-});
 
 export const metadata: Metadata = {
   title: "TLGEF",
@@ -27,11 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.variable} ${playfairDisplay.variable} antialiased`}
-      >
+      <body className="antialiased">
         {children}
-        <Toaster richColors position="top-right" /> 
+        <Toaster richColors position="top-right" />
       </body>
     </html>
   );
